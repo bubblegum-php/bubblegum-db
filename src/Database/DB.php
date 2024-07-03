26ffc96d78e9ae6a79233e024979c1a56ce38dc8
Oleksii Fedorenko <lankrf@gmail.com>
2024-07-03 21:36:14 +0300

    Database core init

---
+++ src/Database/DB.php
@@ -0,0 +1,46 @@
+<?php
+
+class DB {
+    /**
+     * @var PDO|null
+     */
+    protected static ?PDO $pdo;
+
+    /**
+     * @return string
+     */
+    protected static function getDatabasePropertiesFromEnv(): string
+    {
+        return sprintf(
+            "%s:host=%s;port=%d;dbname=%s",
+            env('DATABASE_CONNECTION', 'pgsql'),
+            env('DATABASE_HOST', 'localhost'),
+            env('DATABASE_PORT', 5432),
+            env('DATABASE_DB', 'database')
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public static function initPDO()
+    {
+        self::$pdo = new PDO(self::getDatabasePropertiesFromEnv(), env('DATABASE_USER', 'root'), env('DATABASE_PASSWORD'));
+    }
+
+    public static function exec($sqlStatement): void
+    {
+        self::$pdo->exec($sqlStatement);
+    }
+
+    /**
+     * @param string $tableName
+     * @param string[] $columnSqlParts
+     * @return void
+     */
+    public static function createTable(string $tableName, array $columnSqlParts): void
+    {
+        $columnsSql = implode(',', $columnSqlParts);
+        self::exec("CREATE TABLE $tableName($columnsSql);");
+    }
+}
